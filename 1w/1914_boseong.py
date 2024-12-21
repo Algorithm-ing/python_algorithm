e0a122e4faaaaaadb205dd021d6f294d8eaba2a8
bbosomc <188023651+bbosomc@users.noreply.github.com>
2024-12-22 02:02:59 +0900

    add 1914

---
+++ 1w/1914_boseong.py
@@ -0,0 +1,13 @@
+def hanoi_tower(n, s, e) :
+    if n == 1 :
+        print(s, e)
+        return
+
+    hanoi_tower(n-1, s, 6-s-e)
+    print(s, e)
+    hanoi_tower(n-1, 6-s-e, e)
+
+n = int(input())
+print(2**n-1)
+if n <= 20:
+    hanoi_tower(n, 1, 3)
