32d510763d77956d6ec7ded9dade5525342b73e7
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2024-12-26 12:52:24 +0900

    백준 2493. 탑 - 골드 5단계

---
+++ jiwon_jeong/2week/2493.py
@@ -0,0 +1,16 @@
+# 백준 2493. 탑
+# https://www.acmicpc.net/problem/2493
+import sys
+N = int(sys.stdin.readline().strip())
+towers = list(map(int, sys.stdin.readline().split()))
+def sol(N, towers):
+    result = [0] * N
+    stack = []
+    for i in range(N):
+        while stack and stack[-1][1] < towers[i]:
+            stack.pop()
+        if stack:
+            result[i] = stack[-1][0]
+        stack.append((i + 1, towers[i]))
+    print(' '.join(map(str, result)))
+sol(N=N, towers=towers)
